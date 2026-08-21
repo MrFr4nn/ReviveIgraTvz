aed9741000a297f8698071cc5629a77d5a20ba0d
MrFr4nn <franstanic132@gmail.com>
2026-08-21 12:04:36 +0200

    Promjena enum klase

--- src/main/java/hr/tvz/revive/model/TipRadnika.java
+++ src/main/java/hr/tvz/revive/model/TipRadnika.java
@@ -2,18 +2,8 @@ package hr.tvz.revive.model;
 
 public enum TipRadnika {
 
-    EXPLORER("Kopa kristale, rijetko pronađe i novu kartu"),
-    BUILDER("Sakuplja zupčanike iz rudnika"),
-    SCHOLAR("Prikuplja hranu iz zaliha"),
-    SCIENTIST("Istražuje i donosi bodove");
-
-    private final String opisSposobnosti;
-
-    TipRadnika(String opisSposobnosti) {
-        this.opisSposobnosti = opisSposobnosti;
-    }
-
-    public String getOpisSposobnosti() {
-        return opisSposobnosti;
-    }
+    EXPLORER,
+    BUILDER,
+    SCHOLAR,
+    SCIENTIST
 }
